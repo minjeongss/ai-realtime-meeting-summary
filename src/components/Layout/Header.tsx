989407c7be8c0d600b68df57defe56f6e655a932
minjeongss <dotoriido@gmail.com>
2025-06-06 15:27:59 +0900

    feat: 입장 화면 구현

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <div className="flex flex-row gap-4 items-center pl-10 pr-10 pt-4 pb-4 border-solid border-b-1 border-[#E5E8EB]">
+    <div className="fixed w-full flex flex-row gap-4 items-center pl-10 pr-10 pt-4 pb-4 border-solid border-b-1 border-[#E5E8EB]">
       <img src="/logo.svg" />
       <p className="font-semibold text-lg">Voice Meeting Summary</p>
     </div>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,21 @@
 const Home = () => {
-  return <div>index</div>;
+  return (
+    <div className="w-full min-h-[calc(100vh-40px)] flex flex-col gap-6 justify-center items-center ">
+      <p className="font-semibold text-2xl">Voice Meeting Summary</p>
+      <input
+        className="w-112 flex justify-center items-center text-center p-4 rounded-2xl text-[#4A739C] bg-[#E8EDF5]"
+        type="text"
+        placeholder="Enter your nickname"
+      />
+      <button className="h-12 pl-5 pr-5 rounded-2xl text-[#F7FAFC] bg-[#0D80F2]">
+        Join Meeting
+      </button>
+      <p>
+        Click the button above to join the meeting. Your nickname will be
+        displayed to other participants.
+      </p>
+    </div>
+  );
 };
 
 export default Home;
