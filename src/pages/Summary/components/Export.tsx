342ee05e2450551ec03a542f117e5377233f7d8d
minjeongss <dotoriido@gmail.com>
2025-06-06 19:25:02 +0900

    feat: Shadcn/ui Progress 설치

--- src/pages/Summary/components/Export.tsx
+++ src/pages/Summary/components/Export.tsx
@@ -3,7 +3,7 @@ import CompletePDF from "./CompletePDF";
 import ReadyPDF from "./ReadyPDF";
 
 const Export = () => {
-  const [isComplete, setIsComplete] = useState(false);
+  const [isComplete, setIsComplete] = useState(true);
   return (
     <div className="flex flex-col gap-3 pt-4 pl-4 pr-4">
       <p className="font-semibold text-lg">Export to PDF</p>
